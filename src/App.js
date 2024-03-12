6bd53898cda2c6cf138249ddb01635521856c975
sherdil7 <sherdildev@gmail.com>
2024-03-13 03:16:48 +0500

    updated-one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import  Shop  from './Pages/Shop';
-import ShopCategory from './Pages/ShopCategory';
-import  Product  from './Pages/Product';
-import Cart from './Pages/Cart';
-import LoginSignup from './Pages/LoginSignup';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import mens_banner from "./Components/Assets/banner_mens.png";
+import womens_banner from "./Components/Assets/banner_women.png";
+import kids_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,17 +19,16 @@ function App() {
         <Routes>
           {/* Define your routes using the <Route> component */}
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category='men'/>} />
-          <Route path="/womens" element={<ShopCategory category='women'/>} />
-          <Route path="/kids" element={<ShopCategory category='kid'/>} />
-          <Route path="/products" element={<Product/>} >
-                  <Route path=':productId' element={Product}></Route>
+          <Route path="/men" element={<ShopCategory banner={mens_banner} category="men" />} />
+          <Route path="/women" element={<ShopCategory banner={womens_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
+          <Route path="/products" element={<Product />}>
+            <Route path=":productId" element={Product}></Route>
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
-         
-         
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offer from "../Components/Offers/Offer";
+import NewCollection from "../Components/NewCollections/NewCollection";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
 
- const Shop = () => {
+const Shop = () => {
   return (
-    <div>Shop</div>
-  )
-}
+    <div>
+      <Hero />
+      <Popular />
+      <Offer />
+      <NewCollection />
+      <NewsLetter/>
+    </div>
+  );
+};
 export default Shop;
