a55027732ef6f5df046e04291a4a0ddedceb8cd7
sherdil7 <sherdildev@gmail.com>
2024-04-30 14:37:48 -0700

    frontend-updated

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,16 +3,95 @@ import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
   const [state,setState]=useState("Login")
+
+
+const[formData,setFormData]=useState({
+  username:"",
+  password:"",
+  email:""
+
+})
+
+
+const changeHandler = (e) => {
+  const { name, value } = e.target;
+  setFormData((prevFormData) => ({
+    ...prevFormData,
+    [name]: value,
+  }));
+};
+
+
+
+
+const login=async ()=>{
+console.log("Login Function executed",formData);
+try {
+  const response = await fetch('http://localhost:4000/login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  });
+  
+  const responseData = await response.json();
+
+  if (responseData.success) {
+    localStorage.setItem('auth-token', responseData.token);
+    window.location.replace('/');
+  }else{
+    alert(responseData.errors)
+  }
+} catch (error) {
+  console.error('Error signing up:', error);
+  // Handle error appropriately, such as displaying an error message to the user
+}
+
+
+}
+const signup = async () => {
+  console.log("Signup Function executed", formData);
+  try {
+    const response = await fetch('http://localhost:4000/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    
+    const responseData = await response.json();
+
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace('/');
+    }else{
+      alert(responseData.errors)
+    }
+  } catch (error) {
+    console.error('Error signing up:', error);
+    // Handle error appropriately, such as displaying an error message to the user
+  }
+};
+
+
+
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-         {state==="Sign Up"?<input type="text" placeholder="Your Name" />:<></>} 
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" name="" />
+         {state==="Sign Up"?<input type="text"  placeholder="Your Name" name="username" value={formData.username} onChange={changeHandler}  />:<></>} 
+          <input type="email" placeholder="Email Address"  name="email" value={formData.email} onChange={changeHandler}  />
+          <input type="password"  placeholder="Password" name="password" value={formData.password} onChange={changeHandler} />
         </div>
-        <button>Continue</button>
+        <button onClick={() => { state === 'Login' ? login() : signup() }}>Continue</button>
+
+
         {state==='Sign Up'?<p className="loginsignup-login">
           Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span>
         </p>:<p className="loginsignup-login">
